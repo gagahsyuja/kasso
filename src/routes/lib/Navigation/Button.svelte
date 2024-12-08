e341d17dcbf0f68a57bf5d0148f36445dd0a02fb
gagahsyuja <gghssa1@gmail.com>
2024-12-09 02:18:55 +0700

    Add simple scale animation on click

--- src/routes/lib/Navigation/Button.svelte
+++ src/routes/lib/Navigation/Button.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
     import Fa from "svelte-fa";
+    import { fade } from "svelte/transition";
     let props = $props();
 </script>
 
-<div class="flex flex-col items-center justify-center min-w-24 max-w-28 text-xl">
+<a data-sveltekit-preload-data="tap" href={props.target} class="flex flex-col items-center justify-center min-w-20 min-h-20 max-w-28 text-xl transition-transform active:scale-90 duration-100">
     <Fa icon={props.icon} />
     <span>{props.title}</span>
-</div>
+</a>
