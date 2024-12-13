54698633850611b16c4fe0d9a3d37dd15d0c89d4
gagahsyuja <gghssa1@gmail.com>
2024-12-13 07:29:52 +0700

    Add currency component

---
+++ src/routes/lib/Currency.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    let { amount = 69000, bold = false } = $props();
+    import accounting from "accounting";
+</script>
+
+<span>
+    Rp<span class="{bold ? "font-bold" : ""}">{accounting.formatMoney(amount, "", 2, ".", ",")}</span>
+</span>
