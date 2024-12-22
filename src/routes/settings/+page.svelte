b3a09eae9495d4ce3cfe69eb747e3de10d98b920
gagahsyuja <gghssa1@gmail.com>
2024-12-23 01:41:44 +0700

    Add settings page and logout button

---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import Navigation from "../lib/Navigation/Navigation.svelte";
+    import Main from "../lib/Main.svelte";
+    import Title from "../lib/Title.svelte";
+    import HugeButton from "../lib/HugeButton.svelte";
+</script>
+
+<Navigation />
+<Main>
+    <Title title="Settings" />
+    <HugeButton value="Logout" onclick={() => window.location.href = '/login'} />
+</Main>
