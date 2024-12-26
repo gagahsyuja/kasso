755d717ef87e4604a06fd6979c6c09f2a41706f4
gagahsyuja <gghssa1@gmail.com>
2024-12-26 20:18:28 +0700

    Modify query and handling empty data

--- src/routes/history/+page.svelte
+++ src/routes/history/+page.svelte
@@ -5,6 +5,7 @@
     import Title from "../lib/Title.svelte";
     import Main from "../lib/Main.svelte";
     import Filter from "../lib/Filter.svelte";
+    import Empty from "../lib/Empty.svelte";
     import { fly } from "svelte/transition";
     import { flip } from "svelte/animate";
     import { onMount } from "svelte";
@@ -28,23 +29,15 @@
         return await db.select("SELECT * FROM categories");
     }
 
-    const load = async (): Promise<Array<any>> => {
-
-        const db = await Database.load("sqlite:database.db");
-
-        return await db.select('SELECT * FROM transactions INNER JOIN categories ON transactions.category_id = categories.id WHERE transactions.id IS NOT NULL');
-
-    };
-
     const loadWithFilter = async (type: String, method: String, category: String,
         fromDate: Date | null, toDate: Date | null): Promise<Array<any>> => {
 
         const db = await Database.load("sqlite:database.db");
 
-        let query = 'SELECT * FROM transactions INNER JOIN categories ON transactions.category_id = categories.id WHERE transactions.id IS NOT NULL';
+        let query = 'SELECT * FROM transactions LEFT JOIN categories ON transactions.category_id = categories.id WHERE transactions.id IS NOT NULL';
 
         if (type !== "All") query = `${query} AND transactions.type = "${type}"`;
-        // if (method !== "All") query = `${query} AND method = ${method}`;
+        if (method !== "All") query = `${query} AND transactions.method = "${method}"`;
         if (category !== "All") query = `${query} AND categories.name = "${category}"`;
 
         if (fromDate) {
@@ -87,10 +80,10 @@
             <Filter bind:visible bind:filter { categories } />
         {/if}
     {/await}
-    {#await loadWithFilter(filter.type, "All", filter.category, filter.date.from, filter.date.to) then transactions}
+    {#await loadWithFilter(filter.type, filter.method, filter.category, filter.date.from, filter.date.to) then transactions}
         <div class="overflow-scroll">
             {#each transactions.reverse() as transaction}
-                <div in:fly|global={{ y:50, delay: 0 }} class="py-2">
+                <div in:fly|global={{ y: 50, delay: 0 }} class="py-2">
                     <ListTransaction
                         amount={transaction.amount}
                         type={transaction.type}
@@ -98,6 +91,10 @@
                         description={transaction.description}
                         categoryId={transaction.category_id}
                     />
+                </div>
+            {:else}
+                <div in:fly|global={{ y: 50, delay: 0 }} class="fixed top-0 w-full h-full flex justify-center items-center pr-4">
+                    <Empty value="No transaction has been made." />
                 </div>
             {/each}
         </div>
