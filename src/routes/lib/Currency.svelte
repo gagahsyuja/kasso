6c97f519cc08fc172f1132b2342b7d331582fe24
gagahsyuja <gghssa1@gmail.com>
2024-12-27 14:12:32 +0700

    Add more props

--- src/routes/lib/Currency.svelte
+++ src/routes/lib/Currency.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
     import accounting from "accounting";
 
-    let { amount = 69000, bold = false, subUnit = true } = $props();
+    let { amount = 69000, bold = false, subUnit = true, size = "text-md" } = $props();
     let number: HTMLElement;
 </script>
 
 <span>
-    Rp<span bind:this={number} class="{bold ? "font-bold" : ""}">{accounting.formatMoney(amount, "", subUnit ? 2 : 0, ".", ",")}</span>
+    Rp<span bind:this={number} class="{bold ? "font-bold" : ""} {size}">{accounting.formatMoney(amount, "", subUnit ? 2 : 0, ".", ",")}</span>
 </span>
