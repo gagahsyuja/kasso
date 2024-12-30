5329ad9c08d02b5e820ea93e4123d791a1a0d471
gagahsyuja <gghssa1@gmail.com>
2024-12-31 01:18:59 +0700

    Add other category if it's empty

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -71,9 +71,24 @@
             : false;
     };
 
-    onMount(() => {
+    const setup = async () => {
+        
+        const db = await Database.load("sqlite:database.db");
+
+        const exist: Array<object> = await db.select("SELECT * FROM categories WHERE name = $1", ['other']);
+
+        if (!exist.length) {
+            await db.execute("INSERT INTO categories (name, type) VALUES ($1, $2)", [ 'Other', 'in' ]);
+            await db.execute("INSERT INTO categories (name, type) VALUES ($1, $2)", [ 'Other', 'out' ]);
+        }
+    }
+
+    onMount(async () => {
 
         if (!localStorage.getItem("username")) goto('/login', { replaceState: true });
+
+        await setup();
+
     });
 </script>
 
