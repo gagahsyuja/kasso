c3d05588911ce1c5f8df75cddac989fe863f6ed8
gagahsyuja <gghssa1@gmail.com>
2025-01-06 16:10:48 +0700

    Improve button appearance

--- src/lib/HugeButton.svelte
+++ src/lib/HugeButton.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-    let { value = $bindable('Button'), onclick } = $props();
+    let { value = $bindable('Button'), onclick, bgColor = 'bg-blue-900', color = 'text-white' } = $props();
 </script>
 
 <div class="w-full px-8 py-5">
-    <button onclick={onclick} class="bg-blue-900 text-white font-semibold p-4 w-full rounded-[30px]">{value}</button>
+    <button onclick={onclick} class="{bgColor} {color} font-semibold py-4 w-full rounded-[30px]">{value}</button>
 </div>
